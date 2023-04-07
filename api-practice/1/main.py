3e1443ea53ae4ed4d61fd0c4972e2fe01cf2c389
Kevin Londono <121141072+kevinlondono-chron@users.noreply.github.com>
2023-04-07 23:39:42 +0000

    API Fundamentals with Python

--- api-practice/1/main.py
+++ api-practice/1/main.py
@@ -1,1 +1,25 @@
-print("This is just a test")
+import requests
+
+BASE_ENDPOINT = "https://rickandmortyapi.com/api/character"
+headers = {
+    "api-key": "password" # ENV Var
+}
+
+data = requests.get(url=BASE_ENDPOINT, headers=headers)
+
+if data.status_code == 200:
+    for char in data.json()["results"]:
+        print(char["name"])
+else:
+    data.raise_for_status()
+
+
+# Eaxmple to Create a Character #
+""" data = {"name": "chron", "status": "alive",}
+response = requests.post(url=BASE_ENDPOINT, data=data)
+print(response) 
+ """
+
+
+
+
